d6ac5e9aac563c0fe0cef7b6b8d6fb5ad4959efc
binaythapa <tbinay5@gmail.com>
2024-02-24 17:53:02 +0545

    remove like from admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Category, Tag, Article, Comment, Like,View
+from .models import Category, Tag, Article, Comment, View
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Article)
 admin.site.register(Comment)
-admin.site.register(Like)
 admin.site.register(View)
